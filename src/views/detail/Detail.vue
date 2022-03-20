0b99bd7cd8dfe83e54af92429296123504aa6f8f
橘子 <2370523738@qq.com>
2022-03-20 19:58:49 +0800

    countinue

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,40 +1,65 @@
 <template>
-  <div>
+  <div class="detail">
     <detail-item></detail-item>
-    <detail-swiper :top-images="topImages"></detail-swiper>
+    <scroll class="content">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <base-info :goods="goods" />
+      <detail-shop-info :shop="shop"/>
+    </scroll>
   </div>
 </template>
 
 <script>
-import DetailItem from './childrenItem/DetailItem.vue'
-import { getDetail } from 'network/detail.js'
-import DetailSwiper from './childrenItem/DetailSwiper.vue'
+import DetailItem from "./childrenItem/DetailItem.vue";
+import { getDetail, Goods } from "@/network/detail.js";
+import DetailSwiper from "./childrenItem/DetailSwiper.vue";
+import BaseInfo from "./childrenItem/BaseInfo.vue";
+import Scroll from "@/components/common/Scroll/Scroll";
+import DetailShopInfo from "@/views/detail/childrenItem/DetailShopInfo";
 
 export default {
-  name: 'Detail',
+  name: "Detail",
   data() {
     return {
       iid: null,
-      topImages: []
-    }
+      topImages: [],
+      goods: null,
+      shop: null
+    };
   },
   components: {
     DetailItem,
-    DetailSwiper
+    DetailSwiper,
+    BaseInfo,
+    Scroll,
+    DetailShopInfo
   },
   created() {
-    this.iid = this.$route.params.iid
+      this.iid = this.$route.params.iid;
 
-    getDetail(this.iid).then(res => {
-      console.log(res);
-      this.topImages = res.result.itemInfo.topImages
-
-    })
-
-  }
-}
+      getDetail(this.iid).then((res) => {
+        console.log(res);
+        this.topImages = res.result.itemInfo.topImages;
+        this.goods = new Goods(
+          res.result.itemInfo,
+          res.result.columns,
+          res.result.shopInfo.services
+        );
+        this.shop = res.result.shopInfo
+      });
+    },
+};
 </script>
 
 <style scoped>
-
+  .detail {
+    height: 100vh;
+    background-color: #fff;
+    position: relative;
+    z-index: 1;
+  }
+  .content {
+    height: calc(100% - 44px);
+    background-color: #fff;
+  }
 </style>
---
+++ src/views/detail/childrenItem/DetailShopInfo.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="detail-shop-info">
+    <div class="logo">
+      <img :src="shop.shopLogo" alt="">
+      <p>{{ shop.name }}</p>
+    </div>
+    <div class="shopInfo">
+      <span>{{ shop.cSells | cell }}</span>
+      <p>{{ shop.cGoods }}</p>
+    </div>
+    <div class="shopText">
+      <span>总销量</span>
+      <p>全部宝贝</p>
+    </div>
+    <div class="shopTable">
+      <table class="">
+        <tr v-for="item in shop.score">
+          <td>{{item.name}}</td>
+          <td>{{ item.score }}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailShopInfo",
+  props: {
+    shop: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  filters: {
+    cell(value) {
+      let result = value
+      if(value > 10000) {
+        result = (result / 10000).toFixed(1) + '万'
+        return result
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  position: relative;
+}
+  .logo img {
+    position: absolute;
+    top: 10px;
+    left: 20px;
+    width: 50px;
+    height: 50px;
+  }
+  .logo p {
+    position: absolute;
+    top: 30px;
+    left: 110px;
+    width: 100px;
+  }
+  .shopInfo {
+    position: relative;
+  }
+  .shopInfo span {
+    position: absolute;
+    top: 100px;
+    left: 20px;
+    margin-left: 20px;
+  }
+.shopInfo p {
+  position: absolute;
+  top: 100px;
+  left: 100px;
+  margin-left: 20px;
+}
+.shopText {
+  position: relative;
+}
+.shopText span {
+  position: absolute;
+  top: 135px;
+  left: 35px;
+}
+.shopText p {
+  position: absolute;
+  top: 135px;
+  left: 106px;
+}
+.shopTable {
+  position: relative;
+}
+.shopTable table {
+  position: absolute;
+  top: 82px;
+  right: 37px;
+}
+.shopTable table td {
+  width: 80px;
+  height: 25px;
+}
+.detail-shop-info {
+  border-top: 2px solid #cccccc;
+}
+</style>
